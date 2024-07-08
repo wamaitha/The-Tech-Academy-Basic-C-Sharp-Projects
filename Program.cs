b11879c098f203df921a4ba931a09910a3ce9134
hamzazahid8556205 <129792956+hamzazahid8556205@users.noreply.github.com>
2024-07-09 03:27:17 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Branching_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Requirement 1
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            // Requirement 2
+            Console.Write("Please enter the package weight: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            // Requirement 3
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            // Requirement 4
+            Console.Write("Please enter the package width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            // Requirement 5
+            Console.Write("Please enter the package height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            // Requirement 6
+            Console.Write("Please enter the package length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            // Requirement 7
+            if ((width + height + length) > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                return;
+            }
+
+            // Requirement 8 & 9
+            double quote = (width * height * length * weight) / 100;
+
+            // Requirement 10
+            Console.WriteLine($"Your estimated total for shipping this package is: ${quote:F2}");
+            Console.WriteLine("Thank you!");
+        }
+    }
+}
